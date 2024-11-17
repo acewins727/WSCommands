0294d5d9d709dfc26e92206d4765e507f0a70142
acewins727 <acewins.officiel@gmail.com>
2024-11-17 14:10:10 +0100

    Full code of WSCommands

---
+++ 1.21.1/java/net/acewins/wscommands/command/WSCCommand.java
@@ -0,0 +1,55 @@
+
+package net.acewins.wscommands.command;
+
+import org.checkerframework.checker.units.qual.s;
+
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.common.util.FakePlayerFactory;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.Direction;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.Commands;
+
+import net.acewins.wscommands.procedures.ResetProcedure;
+import net.acewins.wscommands.procedures.ForceRunProcedure;
+
+@EventBusSubscriber
+public class WSCCommand {
+	@SubscribeEvent
+	public static void registerCommand(RegisterCommandsEvent event) {
+		event.getDispatcher().register(Commands.literal("wsc").requires(s -> s.hasPermission(4)).then(Commands.argument("name", EntityArgument.players()).then(Commands.literal("reset").executes(arguments -> {
+			Level world = arguments.getSource().getUnsidedLevel();
+			double x = arguments.getSource().getPosition().x();
+			double y = arguments.getSource().getPosition().y();
+			double z = arguments.getSource().getPosition().z();
+			Entity entity = arguments.getSource().getEntity();
+			if (entity == null && world instanceof ServerLevel _servLevel)
+				entity = FakePlayerFactory.getMinecraft(_servLevel);
+			Direction direction = Direction.DOWN;
+			if (entity != null)
+				direction = entity.getDirection();
+
+			ResetProcedure.execute(entity);
+			return 0;
+		})).then(Commands.literal("forcerun").executes(arguments -> {
+			Level world = arguments.getSource().getUnsidedLevel();
+			double x = arguments.getSource().getPosition().x();
+			double y = arguments.getSource().getPosition().y();
+			double z = arguments.getSource().getPosition().z();
+			Entity entity = arguments.getSource().getEntity();
+			if (entity == null && world instanceof ServerLevel _servLevel)
+				entity = FakePlayerFactory.getMinecraft(_servLevel);
+			Direction direction = Direction.DOWN;
+			if (entity != null)
+				direction = entity.getDirection();
+
+			ForceRunProcedure.execute(entity);
+			return 0;
+		}))));
+	}
+}
---
+++ 1.21.1/java/net/acewins/wscommands/procedures/AddWorldProcedure.java
@@ -0,0 +1,115 @@
+package net.acewins.wscommands.procedures;
+
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.AdvancementHolder;
+
+import javax.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+@EventBusSubscriber
+public class AddWorldProcedure {
+	@SubscribeEvent
+	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		execute(event, event.getEntity().level(), event.getEntity());
+	}
+
+	public static void execute(LevelAccessor world, Entity entity) {
+		execute(null, world, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		File Config = new File("");
+		com.google.gson.JsonObject commands = new com.google.gson.JsonObject();
+		double X = 0;
+		Config = new File((FMLPaths.GAMEDIR.get().toString() + "\\config"), File.separator + "WSCommands.json");
+		X = 1;
+		{
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(Config));
+				StringBuilder jsonstringbuilder = new StringBuilder();
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					jsonstringbuilder.append(line);
+				}
+				bufferedReader.close();
+				commands = new com.google.gson.Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+				if (commands.get("DedicatedMode").getAsBoolean()) {
+					if (!(entity instanceof ServerPlayer _plr3 && _plr3.level() instanceof ServerLevel && _plr3.getAdvancements().getOrStartProgress(_plr3.server.getAdvancements().get(ResourceLocation.parse("wscommands:new_world"))).isDone())) {
+						if (!commands.get("Repeat").getAsBoolean()) {
+							if (entity instanceof ServerPlayer _player) {
+								AdvancementHolder _adv = _player.server.getAdvancements().get(ResourceLocation.parse("wscommands:new_world"));
+								if (_adv != null) {
+									AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+									if (!_ap.isDone()) {
+										for (String criteria : _ap.getRemainingCriteria())
+											_player.getAdvancements().award(_adv, criteria);
+									}
+								}
+							}
+						}
+						for (int index0 = 0; index0 < (int) (commands.size() - 2); index0++) {
+							{
+								Entity _ent = entity;
+								if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+									_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null,
+											4, _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), commands.get((new java.text.DecimalFormat("##.##").format(X))).getAsString());
+								}
+							}
+							X = X + 1;
+						}
+					}
+				} else {
+					if (world.players().size() == 1) {
+						if (!(entity instanceof ServerPlayer _plr10 && _plr10.level() instanceof ServerLevel
+								&& _plr10.getAdvancements().getOrStartProgress(_plr10.server.getAdvancements().get(ResourceLocation.parse("wscommands:new_world"))).isDone())) {
+							if (!commands.get("Repeat").getAsBoolean()) {
+								if (entity instanceof ServerPlayer _player) {
+									AdvancementHolder _adv = _player.server.getAdvancements().get(ResourceLocation.parse("wscommands:new_world"));
+									if (_adv != null) {
+										AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+										if (!_ap.isDone()) {
+											for (String criteria : _ap.getRemainingCriteria())
+												_player.getAdvancements().award(_adv, criteria);
+										}
+									}
+								}
+							}
+							for (int index1 = 0; index1 < (int) (commands.size() - 2); index1++) {
+								{
+									Entity _ent = entity;
+									if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+										_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(),
+												_ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4, _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent),
+												commands.get((new java.text.DecimalFormat("##.##").format(X))).getAsString());
+									}
+								}
+								X = X + 1;
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
